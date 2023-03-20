c6d9b265651fd44d3849e2847f9298f55dd36356
Craig Johnson <crajohns@gmail.com>
2023-03-19 19:16:46 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,8 @@
+variable "region" {
+  type        = string
+  description = "The AWS region where the VPCs will be created"
+}
+
 variable "global_cidr" {
   type        = string
   description = "The CIDR block for the VPCs"
@@ -6,6 +11,11 @@ variable "global_cidr" {
 # Define the number of VPCs to create as a variable
 variable "num_vpcs" {
   type = number
+}
+
+variable "subnets" {
+  type        = map(number)
+  description = "Number of subnets to create per availability zone"
 }
 
 variable "tgw_name" {
